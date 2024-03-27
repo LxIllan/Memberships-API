bb7999ec537e9e0e8bfcea855bdb6777e33ab412
lfillan <lf.illan@mavi.mx>
2024-03-27 15:18:28 -0600

    Add password validation to signUp validator.

--- validators/auth.js
+++ validators/auth.js
@@ -6,7 +6,12 @@ const { body } = require("express-validator");
 exports.signUp = [
     body("name", "Name is required.").notEmpty(),
     body("lastName", "LastName is required.").notEmpty(),
-    body("email", "Email must be a valid email.").isEmail().toLowerCase()
+    body("email", "Email must be a valid email.").isEmail().toLowerCase(),
+    body("password")
+        .isLength({ min: 6 })
+        .withMessage("Password must be at least 6 chars long.")
+        .matches(/\d/)
+        .withMessage("Password must contain a number.")
 ];
 
 
