d2282404c043bdfc8af552b82566aa0a3970defb
lfillan <lf.illan@mavi.mx>
2024-03-27 15:18:52 -0600

    Set strictQuery to false in mongoose.connect() call.

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ const logger = require('./logger');
 
 const connectDB = async () => {
     try {
+        mongoose.set('strictQuery', false);
         const conn = await mongoose.connect(process.env.MONGO_URI)
         logger.info(`DB connection established to ${conn.connection.host}.`)
     } catch (error) {
